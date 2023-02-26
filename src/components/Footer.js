7e6bbd5d7b83eb5e848245e859a5a8d9f2e430f1
Matthew Williams <maw01091989@gmail.com>
2023-02-25 20:56:04 -0800

    added some styling and color changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa';
 function Footer(){
     return (
         <footer>
-            <h2>Footer</h2>
             <a href="https://github.com/matwll" target="_blank" ><FaGithub style={{height: '50px', width: '50px'}} /></a>
+            <h2>Footer</h2>
             <a href="https://www.linkedin.com/in/matthew-williams-9b70881b1/" target="_blank" ><FaLinkedin style={{height: '50px', width: '50px'}}/></a>
         </footer>
     )
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
 
 export default function About() {
   return (
     <div>
-      <h1>About Me</h1>
-      <p>
+      <h2>About Me</h2>
+      <p style={{backgroundColor: '#CECECE', fontSize: '50px'}}>
         {/*about me text here*/}
+        ewhlrw <br></br>
+        whlrwlrhwr'<br></br>
+        hklthwr<br></br>
+        rhasrlkhawrk;lar<br></br>
+        ahlrkhr;ajr<br></br>
+        arhalrkahr;larahrlakrh<br></br>
+
+
       </p>
     </div>
   );
