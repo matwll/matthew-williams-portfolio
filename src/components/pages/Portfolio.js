da4702f28071c292b8843ba49721b58770197cef
Matthew Williams <maw01091989@gmail.com>
2023-02-27 20:53:20 -0800

    changed projects page to utilize cards instead of figures

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -9,108 +9,128 @@ export default function Portfolio() {
     <div>
       <h2>Portfolio</h2>
       <br></br>
-      <figure>
-        <figcaption>The Collar Club</figcaption>
+      <div className="card-group">
+      <div className="card">
+        <h5 className="card-title">The Collar Club</h5>
         <a href="https://calm-ocean-49248.herokuapp.com/" target="_blank">
           <img
             src="/images/project2screenshot.jpg"
             alt="The Collar Club"
             width="100%"
-          />
+            className="card-image-top"
+          ></img>
         </a>
-        <a href="https://github.com/walleric23/the-collar-club" target="_blank">
-          Click here for the github repository
-        </a>
-      </figure>
-      <figure>
-        <figcaption>Meet Brewery</figcaption>
+        <div className="card-body">
+          <p>heres some text about the project</p>
+        </div>
+        <a
+            href="https://github.com/walleric23/the-collar-club"
+            target="_blank"
+          >
+            Click here for the github repository
+          </a>
+      </div>
+      <div className="card">
+      <h5 className="card-title">Meet Brewery</h5>
         <a href="https://matwll.github.io/nearby-breweries/" target="_blank">
           <img
             src="images/brewery-search-screenshot1.png"
             alt="Meet Brewery Search"
             width="100%"
-          />
+            className="card-image-top"
+          ></img>
         </a>
-        <a href="https://github.com/matwll/nearby-breweries" target="_blank">
-        Click here for the github repository
-        </a>
-      </figure>
-      <figure>
-        <figcaption>Weather Dashboard</figcaption>
+        <div className="card-body">
+          <p>heres some text about the project</p>
+        </div>
         <a
-          href="https://matwll.github.io/weather-search-dashboard/"
-          target="_blank"
-        >
+            href="https://github.com/matwll/nearby-breweries" target="_blank"
+          >
+            Click here for the github repository
+          </a>
+      </div>
+      <div className="card">
+      <h5 className="card-title">Weather Dashboard</h5>
+        <a href="https://matwll.github.io/weather-search-dashboard/"
+          target="_blank">
           <img
             src="images/weather-dashboard.png"
             alt="Weather Search Dashboard"
             width="100%"
-          />
+            className="card-image-top"
+          ></img>
         </a>
+        <div className="card-body">
+          <p>heres some text about the project</p>
+        </div>
         <a
-          href="https://github.com/matwll/weather-search-dashboard"
+            href="https://github.com/matwll/weather-search-dashboard"
           target="_blank"
-        >
-          Click here for the github repository
-        </a>
-      </figure>
-      <figure>
-        <figcaption>4th Project</figcaption>
-        <a
-          href=""
-          target="_blank"
-        >
+          >
+            Click here for the github repository
+          </a>
+      </div>
+      {/*<div className="card">
+      <h5 className="card-title">The Collar Club</h5>
+        <a href="https://calm-ocean-49248.herokuapp.com/" target="_blank">
           <img
-            src=""
-            alt=""
+            src="/images/project2screenshot.jpg"
+            alt="The Collar Club"
             width="100%"
-          />
+            className="card-image-top"
+          ></img>
         </a>
+        <div className="card-body">
+          <p>heres some text about the project</p>
+        </div>
         <a
-          href=""
-          target="_blank"
-        >
-          Click here for the github repository
-        </a>
-      </figure>
-      <figure>
-        <figcaption>5th Project</figcaption>
-        <a
-          href=""
-          target="_blank"
-        >
+            href="https://github.com/walleric23/the-collar-club"
+            target="_blank"
+          >
+            Click here for the github repository
+          </a>
+      </div>
+      <div className="card">
+      <h5 className="card-title">The Collar Club</h5>
+        <a href="https://calm-ocean-49248.herokuapp.com/" target="_blank">
           <img
-            src=""
-            alt=""
+            src="/images/project2screenshot.jpg"
+            alt="The Collar Club"
             width="100%"
-          />
+            className="card-image-top"
+          ></img>
         </a>
+        <div className="card-body">
+          <p>heres some text about the project</p>
+        </div>
         <a
-          href=""
-          target="_blank"
-        >
-          Click here for the github repository
-        </a>
-      </figure>
-      <figure>
-        <figcaption>6th Project</figcaption>
-        <a
-          href=""
-          target="_blank"
-        >
+            href="https://github.com/walleric23/the-collar-club"
+            target="_blank"
+          >
+            Click here for the github repository
+          </a>
+      </div>
+      <div className="card">
+      <h5 className="card-title">The Collar Club</h5>
+        <a href="https://calm-ocean-49248.herokuapp.com/" target="_blank">
           <img
-            src=""
-            alt=""
+            src="/images/project2screenshot.jpg"
+            alt="The Collar Club"
             width="100%"
-          />
+            className="card-image-top"
+          ></img>
         </a>
+        <div className="card-body">
+          <p>heres some text about the project</p>
+        </div>
         <a
-          href=""
-          target="_blank"
-        >
-          Click here for the github repository
-        </a>
-      </figure>
+            href="https://github.com/walleric23/the-collar-club"
+            target="_blank"
+          >
+            Click here for the github repository
+          </a>
+      </div> */}
+      </div>
     </div>
   );
 }
