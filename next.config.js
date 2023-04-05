86e5aef5b243fa9dfddc26097363b34e774ba4ac
Matthew Williams <maw01091989@gmail.com>
2023-04-04 19:48:18 -0700

    updated next-config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-// next.config.js
-const isProd = process.env.NODE_ENV === "production";
-
 module.exports = {
-  assetPrefix: isProd ? "/matthew-williams-portfolio/" : "",
+  basePath: "/matthew-williams-portfolio",
+  assetPrefix: "/matthew-williams-portfolio/",
   images: {
     unoptimized: true,
   },
