2a82628a8d6ad034af0473671190c770244c2381
Matthew Williams <maw01091989@gmail.com>
2023-04-04 20:05:42 -0700

    changed image import file paths

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     titles: "Ro-Sham-Bots",
     description:
       "Using React, Node.js, Express, MongoDB, and Socket.io a small group of people built this app. The App is called Ro-SHham-Bots, and it is a simple team building app. There is a realtime chat element for team mates to discuss the patterns for each bot in hopes of getting a 5 win streak. We devided the roles as layout, socket.io, backend database, frontend database, and bot logic primarily.",
-    image: "/images/beat-the-bots.png",
+    image: "images/beat-the-bots.png",
     tags: ["Mongo", "Express", "React", "Node", "Socket.io"],
     source: "https://beat-the-bots.herokuapp.com/dashboard",
     visit: "https://github.com/katbakr/beat-the-bots",
@@ -12,7 +12,7 @@ export const projects = [
   {
     title: "Me and Victoria",
     description: "",
-    image: "/images/meandvictoria.jpg",
+    image: "images/meandvictoria.jpg",
     tags: ["React", "ChatEngine", "Firebase"],
     source: "",
     visit: "",
@@ -21,7 +21,7 @@ export const projects = [
   {
     title: "Doggos",
     description: "",
-    image: "/images/doggos.jpg",
+    image: "images/doggos.jpg",
     tags: ["React", "ChatEngine", "Firebase"],
     source: "",
     visit: "",
@@ -31,7 +31,7 @@ export const projects = [
     titles: "The Collar Club",
     description:
       "This website was built utilizing Handlebars, Node.js, Express & Mysql. It is a user friendly app that allows you to create an account, and either sign up to be a provider or search for providers near you. A provider is someone who is willing to pet sit, walk and/or feed pets.",
-    image: "/images/project2screenshot.jpg",
+    image: "images/project2screenshot.jpg",
     tags: ["Mysql", "Express", "Handlebars", "Node"],
     source: "https://calm-ocean-49248.herokuapp.com/",
     visit: "https://github.com/HunterHarvell/the-collar-club",
@@ -41,7 +41,7 @@ export const projects = [
     title: "Meet Brewery",
     description:
       "This brewery search engine was built using standard HTML, CSS, Javascript, and api calls. This was the first group project I worked on which gave me a whole new set of experience. It helped me realize the importantce of consistant naming conventions, and git management. It utilized localstorage, and I had to put in a sort of catch to handle cases where there were less than the minimum results.",
-    image: "/images/brewery-search-screenshot.png",
+    image: "images/brewery-search-screenshot.png",
     tags: ["HTML", "CSS", "JavaScript"],
     source: "https://matwll.github.io/nearby-breweries/",
     visit: "https://github.com/matwll/nearby-breweries",
@@ -51,7 +51,7 @@ export const projects = [
     title: "Timed Beginner Coding Quiz",
     description:
       "This is an early project I built that utilizes HTML, CSS, and Javascript. This project helped me learn and practice with intervals, as well as deconstructed loop to variable display questions and deduct time if the answer was wrong. I was also able to utilize a very basic toggle to switch back to the main screen after score was submitted.",
-    image: "/images/timed-quiz-screenshot.png",
+    image: "images/timed-quiz-screenshot.png",
     tags: ["React", "ChatEngine", "Firebase"],
     source: "https://github.com/matwll/timed-coding-quiz",
     visit: "https://matwll.github.io/timed-coding-quiz/",
